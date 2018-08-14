5b024464e7d2339fa8a0727da851d5b9bb39566e
Yevnytska Maryna <me_home@ukr.net>
2018-08-14 10:33:28 +0300

    'Second'

--- app.js
+++ app.js
@@ -5,30 +5,43 @@ var MongoClient = require("mongodb").MongoClient;
 var url = 'mongodb://localhost:27017/ myProject';
 var urlencodedParser = bodyParser.urlencoded({extended: true});
 var jsonParser = bodyParser.json();
-
 app.set('port', 3000);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.listen(3000, function (){console.log('App started')});
 app.use(express.static(__dirname + "/public"));
-//Enter all the tasks
-// app.get('/', function(req, response, next){
-// response.send("hello world");
+
+//Output all the tasks to app's interface
+app.post('/', function(req, response, next){
+    console.log("hello world from server");
+    MongoClient.connect(url, {useNewUrlParser: true },function (err,client){
+        var db = client.db("myProject");
+        var collection = db.collection("tasks");
+        collection.find().toArray(function(err, results){
+            response.send({results});
+            client.close();
+        });
+    });
+});
+// app.get('/:id', function(req, response, next){
+//     var id = new objectId(req.params.id);
+//     console.log(id);
 //     MongoClient.connect(url, {useNewUrlParser: true },function (err,client){
 //         var db = client.db("myProject");
 //         var collection = db.collection("tasks");
-//         collection.find().toArray(function(err, results){
-//         response.send({results});
-//         client.close();
+//         collection.findOne({_id: id}).toArray(function(err, results){
+//             response.send({results});
+//             var task=[{results}];
+//             client.close();
 //         });
 //     });
-//
 // });
 
+// Add data to data base
 app.post('/ToDoList', jsonParser, function (request, response){
     if(!request.body) return response.sendStatus(400);
     console.log(request.body);
-    //response.send(`${request.body.task}`);
+    //response.send(`${request.body.task}` + 'This is new value, than we send to server ☺ ☺');
     var taskName = request.body.task;
     MongoClient.connect(url, {useNewUrlParser: true },function (err,client) {
         var db = client.db("myProject");
@@ -49,5 +62,18 @@ app.post('/ToDoList', jsonParser, function (request, response){
 
     });
 });
+
+//update the tasks to data base
+
+app.put('/', function (req, res) {
+    res.send('PUT request to homepage');
+});
+
+//delete the tasks to data base
+
+app.delete('/', function (req, res) {
+    res.send('DELETE request to homepage');
+});
+
 
 
